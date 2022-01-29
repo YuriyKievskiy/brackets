539aacc6a53d19d923cf61b43377039856f48b2a
YuriyKievskiy <kievskiy.y.a@gmail.com>
2022-01-29 16:55:14 +0300

    init: coplete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open_brackets = ['(', '[', '{', '|', '1', '3', '5', '7', '8'];
+  const bad_brackets = ['|', '7', '8']
+  let stack = [];
+  let OBJ_BRACKETS = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++){
+    OBJ_BRACKETS[bracketsConfig[i][1]] = bracketsConfig[i][0];
+   }
+
+  for (let i = 0; i < str.length; i++) {
+    let symbl = str[i];
+
+    if(open_brackets.includes(symbl)) {
+      if(bad_brackets.includes(symbl)){
+        if(stack[stack.length -1] === symbl){
+          stack.pop();
+        }else{
+          stack.push(symbl);
+        }
+      }else{
+        stack.push(symbl);
+      }
+    }else{
+      if(stack.length === 0) {
+        return false;
+      }
+
+      let topElem = stack[stack.length - 1];
+      
+      if(OBJ_BRACKETS[symbl] === topElem) {
+        stack.pop();
+      }else{
+        return false
+      }
+    }
+  }
+  return stack.length === 0;
 }
